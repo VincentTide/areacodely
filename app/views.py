32a2b63adcfe2ac5a68d612f4d22ce624a25ff17
Vincent Zhang <vincenttide@gmail.com>
2015-05-02 15:52:37 -0400

    more changes

--- app/views.py
+++ app/views.py
@@ -33,17 +33,43 @@ def search_post():
         central_office_code = int(number[3:6])
         location = Telephone.query.filter_by(area_code=area_code,
                                              central_office_code=central_office_code).first()
-        # If we don't find it
+        # Format dates for human readers
+        if location.date_assigned is not None:
+            location.date_assigned = location.date_assigned.strftime('%m/%d/%Y')
+        else:
+            location.date_assigned = ""
+        if location.date_effective is not None:
+            location.date_effective = location.date_effective.strftime('%m/%d/%Y')
+        else:
+            location.date_effective = ""
+
+        # If we have no data, show as blank
+        if location.full_name is None:
+            location.full_name = ""
+        if location.metro is None:
+            location.metro = ""
+
+        # Google maps search query
+        query = "%s %s" % (location.full_name, location.state)
+        query = query.split()
+        query = "+".join(query)
+        google_maps = "http://maps.google.com/?q=%s" % query
+
         if location is None:
             # tell user we didnt find it
-            flash("No data found for (NPA) NXX")
+            error_msg = "No data found for %s %s" % (area_code, central_office_code)
+            flash(error_msg)
             return redirect(url_for('index'))
         else:
             # give user the data
             return render_template('search.html',
                                    location=location,
-                                   form=form)
-    return render_template('search.html', location=None, form=form)
+                                   form=form,
+                                   google_maps=google_maps)
+    return render_template('search.html',
+                           location=None,
+                           form=form,
+                           google_maps=None)
 
 
 
