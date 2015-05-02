c5b95d373a86054c6ad42889fd54c0c77ee047cb
Vincent Zhang <vincenttide@gmail.com>
2015-05-02 16:51:43 -0400

    ready

--- app/views.py
+++ app/views.py
@@ -21,7 +21,8 @@ def contact():
 
 @app.route('/search', methods=['GET'])
 def search():
-    return render_template('search.html')
+    form = TelephoneForm()
+    return render_template('search.html', form=form)
 
 @app.route('/search', methods=['POST'])
 def search_post():
@@ -33,43 +34,51 @@ def search_post():
         central_office_code = int(number[3:6])
         location = Telephone.query.filter_by(area_code=area_code,
                                              central_office_code=central_office_code).first()
-        # Format dates for human readers
-        if location.date_assigned is not None:
-            location.date_assigned = location.date_assigned.strftime('%m/%d/%Y')
-        else:
-            location.date_assigned = ""
-        if location.date_effective is not None:
-            location.date_effective = location.date_effective.strftime('%m/%d/%Y')
-        else:
-            location.date_effective = ""
-
-        # If we have no data, show as blank
-        if location.full_name is None:
-            location.full_name = ""
-        if location.metro is None:
-            location.metro = ""
-
-        # Google maps search query
-        query = "%s %s" % (location.full_name, location.state)
-        query = query.split()
-        query = "+".join(query)
-        google_maps = "http://maps.google.com/?q=%s" % query
-
+        # Tell user we didnt find any results
+        entry = {}
         if location is None:
-            # tell user we didnt find it
-            error_msg = "No data found for %s %s" % (area_code, central_office_code)
-            flash(error_msg)
-            return redirect(url_for('index'))
+            entry = {
+                "area_code": area_code,
+                "central_office_code": central_office_code
+            }
+            return render_template('search.html',
+                                   form=form,
+                                   entry=entry)
+
+        # A successful result is found
         else:
-            # give user the data
+            # Format dates for human readers
+            if location.date_assigned is not None:
+                location.date_assigned = location.date_assigned.strftime('%m/%d/%Y')
+            else:
+                location.date_assigned = ""
+            if location.date_effective is not None:
+                location.date_effective = location.date_effective.strftime('%m/%d/%Y')
+            else:
+                location.date_effective = ""
+
+            # If we have no data, show as blank
+            if location.full_name is None:
+                location.full_name = ""
+            if location.metro is None:
+                location.metro = ""
+
+            # Google maps search query
+            query = "%s %s" % (location.full_name, location.state)
+            query = query.split()
+            query = "+".join(query)
+            google_maps = "http://maps.google.com/?q=%s" % query
+
             return render_template('search.html',
                                    location=location,
                                    form=form,
-                                   google_maps=google_maps)
+                                   google_maps=google_maps,
+                                   entry=entry)
     return render_template('search.html',
                            location=None,
                            form=form,
-                           google_maps=None)
+                           google_maps=None,
+                           entry=None)
 
 
 
